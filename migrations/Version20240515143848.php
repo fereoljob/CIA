64251ba130f2bd150c8fb78e02aa49bd59751e33
diyeba1003 <diyaba1003@gmail.com>
2024-05-15 19:39:30 +0200

    Ajout du fichier de migration

--- migrations/Version20240515143848.php
+++ migrations/Version20240515143848.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240515141250 extends AbstractMigration
+final class Version20240515143848 extends AbstractMigration
 {
     public function getDescription(): string
     {
